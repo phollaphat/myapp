9bf88e784a834f9c823943f1513fb1ea06f2faee
phollaphat <phollaphat.s@ku.th>
2023-07-03 11:56:50 +0700

    initial commit : add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,15 @@
+package myapp;
+
+public class Cat {
+    public String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+}
